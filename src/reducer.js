141283d7f617a6e7b0afa83e16af188b9c0c4cb4
TraderCodes <85665271+TraderCodes@users.noreply.github.com>
2022-05-16 04:37:27 -0700

    Reducer set done

---
+++ src/reducer.js
@@ -0,0 +1,9 @@
+// state = current state before the update
+// action = what we trying to do
+
+
+const reducer = (state, action) =>{
+    // return state if nothing match
+    return state 
+}
+export default reducer
